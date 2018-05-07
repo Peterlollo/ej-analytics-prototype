75c47a306f3d77a7f276a9ad0446b8761fe37487
Peter Lollo <peterlollo@Peters-MBP.home>
2018-05-07 20:08:25 +0200

    Add basic styles to pages page

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div v-if='pagePathFromParamStatus === "success"'>
-      <h1>Page: {{ isViewingPage.path }}</h1>
-      <h2>Important Visitors: {{ currentPageWithTimesKeyProviders.length }}</h2>
+      <h1 class='title'>Page: {{ isViewingPage.path }}</h1>
+      <h1 class='visitors'>Important Visitors: {{ currentPageWithTimesKeyProviders.length }}</h1>
       <ul>
         <li v-for='providerTime in currentPageWithTimesKeyProviders' v-bind:key='providerTime[0]'>
           <div><strong>Provider:</strong> {{ getProviderFromID(providerTime[0]).name }}</div>
@@ -11,7 +11,7 @@
           <button v-on:click='whitelistAddOrRemoveProvider({ action: "remove", id: providerTime[0] })'>Remove From Whitelist</button>
         </li>
       </ul>
-      <h2>NON Important Visitors: {{ currentPageWithTimesNotKeyProviders.length }}</h2>
+      <h1 class='visitors'>Non-Important Visitors: {{ currentPageWithTimesNotKeyProviders.length }}</h1>
       <ul>
         <li v-for='providerTime in currentPageWithTimesNotKeyProviders' v-bind:key='providerTime[0]'>
           <div><strong>Provider:</strong> {{ getProviderFromID(providerTime[0]).name }}</div>
@@ -63,9 +63,19 @@ export default {
 </script>
 
 <style scoped>
-
+.title {
+  margin-top: 50px;
+}
+.visitors {
+  color: rgb(0, 132, 137);
+  margin-top: 60px;
+}
+h1 {
+  font-weight: 700;
+}
 ul {
   border: solid 1px #dce0e0;
+  font-size: 16px;
 }
 li {
   padding: 20px;
@@ -73,13 +83,25 @@ li {
   margin-left: 10px;
   margin-right: 10px;
 }
-li:hover {
-  cursor: pointer;
-}
 .pageviews, .pageviews li {
   border: none;
 }
 .pageviews li {
   background-color: #eee;
+}
+li > * {
+  margin: 5px;
+}
+button {
+  font-size: 14px;
+  font-weight: 700;
+  padding: 10px;
+  border-radius: 4px;
+  margin: 10px;
+  background-color: #384249;
+  color: #fff;
+}
+button:hover {
+  cursor: pointer;
 }
 </style>
